7cbd1aa9b64611fcaf8a87e1ff9f9b27f9eee439
Souvik <souvik13109@gmail.com>
2025-04-19 00:20:23 +0530

    Refactor Book class to encapsulate properties and add validation for Title and Author

--- Application/Book.cs
+++ Application/Book.cs
@@ -1,10 +1,35 @@
+using System;
+
 namespace LibrarySystem
 {
     public class Book
     {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public bool IsBorrowed { get; set; }
+        private string _title;
+        private string _author;
+
+        public string Title 
+        { 
+            get { return _title; }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Title cannot be empty.");
+                _title = value;
+            }
+        }
+
+        public string Author 
+        { 
+            get { return _author; }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Author cannot be empty.");
+                _author = value;
+            }
+        }
+
+        public bool IsBorrowed { get; private set; }
 
         public Book(string title, string author, bool isBorrowed = false)
         {
@@ -12,5 +37,23 @@ namespace LibrarySystem
             Author = author;
             IsBorrowed = isBorrowed;
         }
+
+        // Borrow the book
+        public void BorrowBook()
+        {
+            if (IsBorrowed)
+                throw new InvalidOperationException("This book is already borrowed.");
+            IsBorrowed = true;
+        }
+
+        // Return the book
+        public void ReturnBook()
+        {
+            if (!IsBorrowed)
+                throw new InvalidOperationException("This book is not borrowed.");
+            IsBorrowed = false;
+        }
+
+     
     }
 }
